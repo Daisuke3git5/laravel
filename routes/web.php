d4b99ba798a8a2732501b39dc6bd8af254279f0b
Daisuke3git5 <lupi3daisuu@gmail.com>
2020-12-17 05:32:17 +0000

    NewsPost

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' =>'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
 });
 
 //Q3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 
@@ -32,7 +33,9 @@ Route::group(['prefix' => 'admin'], function() {
 //admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 Auth::routes();
