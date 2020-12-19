2f523ec9635d15a4e3527f263482b3cccd15c2bd
Daisuke3git5 <lupi3daisuu@gmail.com>
2020-12-19 10:11:03 +0000

    Migration-Validation

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -4,14 +4,14 @@
     @extends('layouts.admin')
     
     {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-    @section('title', 'ニュースの新規作成')
+    @section('title', 'プロフィールの新規作成')
 
     {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
     @section('content')
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
-                    <h2>ニュース新規作成</h2>
+                    <h2>プロフィールの新規作成</h2>
                     <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-date">
                         
                         @if(count($errors) > 0)
@@ -24,25 +24,25 @@
                         <div class="form-group row">
                             <label class="col-md-2">氏名(name)</label>
                             <div class="col-md-10">
-                                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-2">性別(gender)</label>
                             <div class="col-md-10">
-                                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                                <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-2">趣味(hobby)</label>
                             <div class="col-md-10">
-                                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                                <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-2">自己紹介欄(introduction)</label>
                             <div class="col-md-10">
-                                <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                                <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                             </div>
                         </div>
                         {{ csrf_field() }}
